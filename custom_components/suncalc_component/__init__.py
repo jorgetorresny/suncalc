e7a1162b4031bd8a56d34d732aafc80577bbf30a
jorgetorresny <jorge.torresny@gmail.com>
2024-07-22 23:18:35 -0500

    Update __init__.py

--- custom_components/suncalc_component/__init__.py
+++ custom_components/suncalc_component/__init__.py
@@ -16,9 +16,7 @@ async def async_setup(hass: HomeAssistant, config: dict):
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up SunCalc from a config entry."""
     hass.data[DOMAIN][entry.entry_id] = entry.data
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
