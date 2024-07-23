ffb60bdcba4b2939eaa30cf4c96f9c44ec5c7f12
jorgetorresny <jorge.torresny@gmail.com>
2024-07-22 23:19:11 -0500

    Update sensor.py

--- custom_components/suncalc_component/sensor.py
+++ custom_components/suncalc_component/sensor.py
@@ -11,17 +11,22 @@ _LOGGER = logging.getLogger(__name__)
 
 SCAN_INTERVAL = timedelta(hours=12)
 
+SENSOR_TYPES = {
+    "dawn": "Dawn",
+    "sunrise": "Sunrise",
+    "solarNoon": "Culmination",
+    "sunset": "Sunset",
+    "dusk": "Dusk",
+    "daylight_duration": "Daylight Duration"
+}
+
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the sensor platform."""
     latitude = hass.config.latitude
     longitude = hass.config.longitude
     async_add_entities([
-        SunCalcSensor(latitude, longitude, "dawn"),
-        SunCalcSensor(latitude, longitude, "sunrise"),
-        SunCalcSensor(latitude, longitude, "solarNoon"),
-        SunCalcSensor(latitude, longitude, "sunset"),
-        SunCalcSensor(latitude, longitude, "dusk"),
-        SunCalcSensor(latitude, longitude, "daylight_duration")
+        SunCalcSensor(latitude, longitude, sensor_type)
+        for sensor_type in SENSOR_TYPES
     ])
 
 async def async_setup_entry(hass, entry, async_add_entities):
@@ -29,12 +34,8 @@ async def async_setup_entry(hass, entry, async_add_entities):
     latitude = entry.data[CONF_LATITUDE]
     longitude = entry.data[CONF_LONGITUDE]
     async_add_entities([
-        SunCalcSensor(latitude, longitude, "dawn"),
-        SunCalcSensor(latitude, longitude, "sunrise"),
-        SunCalcSensor(latitude, longitude, "solarNoon"),
-        SunCalcSensor(latitude, longitude, "sunset"),
-        SunCalcSensor(latitude, longitude, "dusk"),
-        SunCalcSensor(latitude, longitude, "daylight_duration")
+        SunCalcSensor(latitude, longitude, sensor_type)
+        for sensor_type in SENSOR_TYPES
     ])
 
 class SunCalcSensor(SensorEntity):
@@ -45,6 +46,7 @@ class SunCalcSensor(SensorEntity):
         self._latitude = latitude
         self._longitude = longitude
         self._sensor_type = sensor_type
+        self._name = SENSOR_TYPES[sensor_type]
         self._state = None
         self._attributes = {}
         self.update()
@@ -52,7 +54,7 @@ class SunCalcSensor(SensorEntity):
     @property
     def name(self):
         """Return the name of the sensor."""
-        return f'SunCalc {self._sensor_type.capitalize()} Sensor'
+        return f'SunCalc {self._name} Sensor'
 
     @property
     def state(self):
@@ -66,13 +68,16 @@ class SunCalcSensor(SensorEntity):
 
     def update(self):
         """Fetch new state data for the sensor."""
-        times = suncalc.get_times(datetime.now(), self._latitude, self._longitude)
-        if self._sensor_type in times:
-            self._state = times[self._sensor_type].strftime('%H:%M:%S')
-        elif self._sensor_type == "daylight_duration":
-            self._state = str(times['sunset'] - times['sunrise'])
-        
-        self._attributes.update({
-            "latitude": self._latitude,
-            "longitude": self._longitude
-        })
+        try:
+            times = suncalc.get_times(datetime.now(), self._latitude, self._longitude)
+            if self._sensor_type in times:
+                self._state = times[self._sensor_type].strftime('%H:%M:%S')
+            elif self._sensor_type == "daylight_duration":
+                self._state = str(times['sunset'] - times['sunrise'])
+            
+            self._attributes.update({
+                "latitude": self._latitude,
+                "longitude": self._longitude
+            })
+        except Exception as e:
+            _LOGGER.error(f"Error updating SunCalc sensor {self._name}: {e}")
