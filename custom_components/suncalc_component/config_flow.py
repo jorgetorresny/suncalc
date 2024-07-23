c0335f42506405c253741238080a6bc331bdd930
jorgetorresny <jorge.torresny@gmail.com>
2024-07-22 23:06:09 -0500

    Update config_flow.py

--- custom_components/suncalc_component/config_flow.py
+++ custom_components/suncalc_component/config_flow.py
@@ -1,6 +1,5 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import callback
 from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE
 import homeassistant.helpers.config_validation as cv
 
@@ -11,17 +10,16 @@ class SunCalcConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for SunCalc."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        errors = {}
         if user_input is not None:
             return self.async_create_entry(title="SunCalc", data=user_input)
 
         return self.async_show_form(
-            step_id="user", data_schema=vol.Schema({
+            step_id="user",
+            data_schema=vol.Schema({
                 vol.Required(CONF_LATITUDE): cv.latitude,
                 vol.Required(CONF_LONGITUDE): cv.longitude,
-            }), errors=errors
+            })
         )
